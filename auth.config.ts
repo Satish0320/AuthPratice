ac2e8c218e73c08a1edb959932074d18f72756ec
satish0320 <rexmax2468@gmail.com>
2025-03-09 00:22:53 +0530

    google completed

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,8 @@
 import Google from "next-auth/providers/google";
 import Credentials from "next-auth/providers/credentials";
 import type { NextAuthConfig } from "next-auth";
+// console.log(process.env.GOOGLE_CLIENT_ID);
+// console.log(process.env.GOOGLE_CLIENT_SECRET);
 
 export default {
     providers: [
